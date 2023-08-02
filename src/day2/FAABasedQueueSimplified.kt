f005f5f5f7957736918b947be61cbebbaf37e3d8
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2023-08-02 12:44:02 +0200

    complete FAABasedQueueSimplified

--- src/day2/FAABasedQueueSimplified.kt
+++ src/day2/FAABasedQueueSimplified.kt
@@ -9,26 +9,38 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     private val deqIdx = atomic(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.value
-        enqIdx.value = i + 1
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray[i].value = element
+        while (true) {
+            val i = enqIdx.getAndIncrement()
+            if (infiniteArray[i].compareAndSet(null, element)) {
+                return
+            }
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.value <= deqIdx.value) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.value
-        deqIdx.value = i + 1
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray[i].value as E
+        while (true) {
+            // Is this queue empty?
+            if (!shouldTryDeque()) {
+                return null
+            }
+
+            val i = deqIdx.getAndIncrement()
+            val element = infiniteArray[i].getAndSet(POISONED)
+            if (element != null && element != POISONED) {
+                return element as E
+            }
+        }
+    }
+
+    private fun shouldTryDeque(): Boolean {
+        while (true) {
+            val curEnqIdx = enqIdx.value
+            val curDeqIdx = deqIdx.value
+
+            if (curEnqIdx != enqIdx.value) continue
+            return curEnqIdx >= curDeqIdx
+        }
     }
 }
 
